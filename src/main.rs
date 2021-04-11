3189c4eab83a407eeaab27edbe0951b4f5313dfb
randymaonyan <randymaonyan@outlook.jp>
2021-04-11 17:25:14 +0900

    delete Result returns

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 mod structs;
 use bitflyer_ticker::stream_ticker;
 
-fn main() -> anyhow::Result<()> {
+fn main() {
     if let Err(e) = stream_ticker() {
         eprintln!("Error: {}", e);
         std::process::exit(1);
     }
-
-    Ok(())
 }
